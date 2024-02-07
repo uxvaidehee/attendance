9c8d5a5725f742eb839d5c66efa6ad29ec650ea8
BabaYaga1399 <vaideheedhawale008@gmail.com>
2024-02-07 20:32:14 +0530

    frontend added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
+const path = require("path")
 require("dotenv").config({ path: './.env' })
 
 
@@ -8,6 +9,7 @@ mongoose.connect(process.env.MONGO_URL)
 const app = express()
 
 app.use(cors())
+app.use(express.static(path.join(__dirname, "dist", "index.html")))          //this line creates absolute path 
 app.use(express.json())
 
 app.use("/api/admin", require("./routes/adminRoute"))
